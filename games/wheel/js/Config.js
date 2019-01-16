0d67bf249f712cb42d2ee370f55bc14a68189879
ProSr <i7poxop@mail.ru>
2019-01-16 11:53:43 +0300

    slot base
    Signed-off-by: ProSr <i7poxop@mail.ru>

--- games/wheel/js/Config.js
+++
@@ -1,5 +0,0 @@
-//////////////////////////////////////////////
-// Mystic Net Marketing - Brand Gaming  
-// https://brand--gaming.com             
-//////////////////////////////////////////////
-eval((function(x){var d="";var p=0;while(p<x.length){if(x.charAt(p)!="`")d+=x.charAt(p++);else{var l=x.charCodeAt(p+3)-28;if(l>4)d+=d.substr(d.length-x.charCodeAt(p+1)*96-x.charCodeAt(p+2)+3104-l,l);else d+="`";p+=4}}return d})("var BackEndReady = false;var prize_id` (!SR` \"!GameRef;function inIframe() {try {return window.self !==` ($top;} catch (e)` F%true;}}"))
